fe38871379052394618c99d8752404cbcae2edd7
pengchao909 <pengchao840421@163.com>
2020-03-07 20:37:05 +0800

    init version

---
+++ algorithm-study/src/test/java/com/charles/algorithm/algorithmstudy/AlgorithmStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.algorithm.algorithmstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
